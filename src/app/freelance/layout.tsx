b1b312202d5cc99317db8042bf10f0b88830b0d5
akg2011-code <akg201194@gmail.com>
2024-08-17 19:56:35 +0300

    feat: Add new UI elements for footer
    Adds footer components and translations for:
    
    - Contact information
    - Key links (Terms & Conditions, Who We Are, etc.)
    - Service categories

---
+++ src/app/freelance/layout.tsx
@@ -0,0 +1,155 @@
+"use client";
+import FooterFreelancer from "@/components/layoutFreelancer/footer";
+import {
+  Avatar,
+  Button,
+  Divider,
+  Layout,
+  Menu,
+  MenuProps,
+  Select,
+  Space,
+} from "antd";
+import Search, { SearchProps } from "antd/es/input/Search";
+import { useTranslations } from "next-intl";
+const { Header, Content, Footer } = Layout;
+import Image from "next/image";
+import { CiMail } from "react-icons/ci";
+import { FaRegUserCircle } from "react-icons/fa";
+import { IoNotificationsOutline } from "react-icons/io5";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const t = useTranslations();
+  const items1: MenuProps["items"] = [
+    {
+      key: "1",
+      label: t("musicAndAudio"),
+    },
+    {
+      key: "2",
+      label: t("writingAndTranslate"),
+    },
+    {
+      key: "3",
+      label: t("marketing"),
+    },
+    {
+      key: "4",
+      label: t("videoAndMotion"),
+    },
+    {
+      key: "5",
+      label: t("programmingAndTechnology"),
+    },
+    {
+      key: "6",
+      label: t("designAndCreative"),
+    },
+    {
+      key: "7",
+      label: t("managmentAndBusiness"),
+    },
+    {
+      key: "8",
+      label: t("consulting"),
+    },
+    {
+      key: "9",
+      label: t("other"),
+    },
+  ];
+  const options = [
+    {
+      value: "1",
+      label: t("jobs"),
+    },
+    {
+      value: "2",
+      label: t("services"),
+    },
+  ];
+  const onSearch: SearchProps["onSearch"] = (value, _e, info) =>
+    console.log(info?.source, value);
+
+  return (
+    <>
+      <Layout>
+        <Header
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            backgroundColor: "white",
+          }}
+        >
+          <div>
+            <Space size={"middle"}>
+              <Avatar icon={<FaRegUserCircle />} size={24} />
+              <IoNotificationsOutline size={24} />
+              <CiMail size={24} />
+              <Space.Compact>
+                <Select defaultValue="1" options={options} />
+                <Search
+                  placeholder={t("search")}
+                  allowClear
+                  onSearch={onSearch}
+                  style={{ width: 200 }}
+                />
+              </Space.Compact>
+            </Space>
+          </div>
+          <div className="logo">
+            <Space size={"large"}>
+              <a
+                href="#"
+                className="font-[Tajawal] text-[15px] font-bold leading-[24px] tracking-[0.5px] text-[#1B2D5E]"
+              >
+                {t("services")}
+              </a>
+              <a
+                href="#"
+                className="font-[Tajawal] text-[15px] font-bold leading-[24px] tracking-[0.5px] text-[#1B2D5E]"
+              >
+                {t("jobs")}
+              </a>
+              <Image
+                src="/images/logo.svg"
+                alt="banner"
+                width={79}
+                height={32}
+              />
+            </Space>
+          </div>
+        </Header>
+        <Header
+          style={{
+            backgroundColor: "white",
+          }}
+        >
+          <Divider
+            style={{ backgroundColor: "#DEEAFF", height: "1px", margin: 0 }}
+          />
+          <Menu
+            mode="horizontal"
+            items={items1}
+            style={{
+              flex: 1,
+              minWidth: 0,
+              backgroundColor: "white",
+            }}
+          />
+        </Header>
+        <Content style={{ padding: "0 48px", minHeight: "52vh" }}>
+          {children}
+        </Content>
+        <Footer style={{ margin: 0, padding: 0 }}>
+          <FooterFreelancer></FooterFreelancer>
+        </Footer>
+      </Layout>
+    </>
+  );
+}
---
+++ src/components/layoutFreelancer/footer.tsx
@@ -0,0 +1,54 @@
+import { useTranslations } from "next-intl";
+import React from "react";
+
+const FooterFreelancer = () => {
+  const t = useTranslations();
+  return (
+    <div className="h-[227px] bg-[#7179CE] flex py-5 px-2 w-full justify-around items-center">
+      <div className="flex flex-col">
+        <h3 className="font-[Tajawal] text-[16px] font-medium leading-[17.6px] text-white py-4">
+          {t("ImportantLinks")}
+        </h3>
+        <p className="font-[Tajawal] text-[16px] font-normal leading-[22.4px] text-[#E2E8F0] py-2">
+          {t("Home")}
+        </p>
+        <p className="font-[Tajawal] text-[16px] font-normal leading-[22.4px] text-[#E2E8F0] py-2">
+          {t("whoWe")}
+        </p>
+        <p className="font-[Tajawal] text-[16px] font-normal leading-[22.4px] text-[#E2E8F0] py-2">
+          {t("Services")}
+        </p>
+      </div>
+      <div className="flex flex-col">
+        <h3 className="font-[Tajawal] text-[16px] font-medium leading-[17.6px] text-white py-4">
+          {t("support")}
+        </h3>
+        <p className="font-[Tajawal] text-[16px] font-normal leading-[22.4px] text-[#E2E8F0] py-2">
+          {t("commanQuestions")}
+        </p>
+        <p className="font-[Tajawal] text-[16px] font-normal leading-[22.4px] text-[#E2E8F0] py-2">
+          {t("technicalSupport")}
+        </p>
+        <p className="font-[Tajawal] text-[16px] font-normal leading-[22.4px] text-[#E2E8F0] py-2">
+          {t("termsAndConditions")}
+        </p>
+      </div>
+      <div className="flex flex-col">
+        <h3 className="font-[Tajawal] text-[16px] font-medium leading-[17.6px] text-white py-4">
+          {t("contactUs")}
+        </h3>
+        <p className="font-[Tajawal] text-[16px] font-normal leading-[22.4px] text-[#E2E8F0] py-2">
+          {t("email")} : pZmCt@example.com
+        </p>
+        <p className="font-[Tajawal] text-[16px] font-normal leading-[22.4px] text-[#E2E8F0] py-2">
+          {t("phone")} : +966 123456789
+        </p>
+        <p className="font-[Tajawal] text-[16px] font-normal leading-[22.4px] text-[#E2E8F0] py-2">
+          {t("address")} : المملكة العربية السعودية
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default FooterFreelancer;
