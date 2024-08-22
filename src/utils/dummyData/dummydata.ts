1eced376dca81a2a210fd1b72b0c334300ffc5a7
Karam Shaarawy <karam.shaarawy@gmail.com>
2024-08-22 23:06:36 +0300

    dummy data

--- src/utils/dummyData/dummydata.ts
+++ src/utils/dummyData/dummydata.ts
@@ -1,6 +1,6 @@
 const skillsOptions = [{}];
 
-for (let i = 10; i < 36; i++) {
+for (let i = 0; i < 5; i++) {
   skillsOptions.push({
     className:
       "bg-[rgba(236,_242,_255,_1)] rounded-[28px] flex items-center text-[18px]",
@@ -8,4 +8,21 @@ for (let i = 10; i < 36; i++) {
     value: `skill ${i}`,
   });
 }
-export { skillsOptions };
+const servicesOptions = [{}];
+
+for (let i = 0; i < 5; i++) {
+  let nestedServices = [];
+  for (let ii = 0; ii < 5; ii++) {
+    nestedServices.push({
+      title: `الخدمة ${ii}`,
+      value: `service ${Math.random()}`,
+      key: `${Math.random()}`,
+    });
+  }
+  servicesOptions.push({
+    title: `الخدمة ${i}`,
+    value: `service ${i}`,
+    children: nestedServices,
+  });
+}
+export { skillsOptions, servicesOptions };
