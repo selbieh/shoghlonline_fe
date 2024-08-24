13f37219ed6f1791f9f989e35e514d2faec4a347
Karam Shaarawy <karam.shaarawy@gmail.com>
2024-08-24 04:42:10 +0300

    fix: restructuring the hom page to utilize tabs component

--- src/app/freelance/page.tsx
+++ src/app/freelance/page.tsx
@@ -1,10 +1,45 @@
+"use client";
+import AuthGuard from "@/components/authenticationHOC/authenticated";
 import Advertise from "@/components/freelance/homePage/advertise";
+import BookmarkedGigs from "@/components/freelance/homePage/bookmarkedGigs";
 import CompleteYourProfile from "@/components/freelance/homePage/completeYourProfile";
-import GigOffer from "@/components/freelance/homePage/gigOffer";
+import GigsSuitsYou from "@/components/freelance/homePage/gigsSuitsYou";
 import InviteFriends from "@/components/freelance/homePage/inviteFriends";
+import { Tabs, TabsProps } from "antd";
+import { useTranslations } from "next-intl";
+import Image from "next/image";
 import React from "react";
 
 const Freelance = () => {
+  const t = useTranslations();
+
+  const tabsItems: TabsProps["items"] = [
+    {
+      key: "1",
+      label: (
+        <div className="w-[154px] h-[36px] px-[12px] flex items-center justify-center text-[16px]">
+          {t("gigsThatMaySuitsYou")}
+        </div>
+      ),
+      children: <BookmarkedGigs />,
+    },
+    {
+      key: "2",
+      label: (
+        <div className="w-[154px] h-[36px] px-[12px] flex items-center justify-center text-[16px]">
+          <Image
+            src={"/icons/bookmark.svg"}
+            alt="bookmark"
+            width={24}
+            height={24}
+          />
+          <span>{t("bookmarkedGigs")}</span>
+        </div>
+      ),
+      children: <GigsSuitsYou />,
+    },
+  ];
+
   return (
     <div className="flex flex-row">
       <div>
@@ -12,18 +47,11 @@ const Freelance = () => {
         <InviteFriends />
         <Advertise />
       </div>
-      <div className="w-full">
-        <GigOffer data={null} />
-        <GigOffer data={null} />
-        <GigOffer data={null} />
-        <GigOffer data={null} />
-        <GigOffer data={null} />
-        <GigOffer data={null} />
-        <GigOffer data={null} />
-        <GigOffer data={null} />
+      <div>
+        <Tabs items={tabsItems} />
       </div>
     </div>
   );
 };
 
-export default Freelance;
+export default AuthGuard(Freelance);
