5186d32c8f2faeffe5b796f352739b22dd3e22ce
Karam Shaarawy <karam.shaarawy@gmail.com>
2024-08-27 21:30:35 +0300

    adding experience to the freelancer profile

--- src/components/completeProfile/addExperienceModal.tsx
+++ src/components/completeProfile/addExperienceModal.tsx
@@ -1,6 +1,13 @@
-import { Checkbox, DatePicker, Form, Input, Modal } from "antd";
+import { RootState, useAppDispatch } from "@/store/rootReducer";
+import { Checkbox, DatePicker, Form, Input, message, Modal } from "antd";
 import { useTranslations } from "next-intl";
 import React, { Fragment } from "react";
+import { useSelector } from "react-redux";
+import dayjs from "dayjs";
+import { PatchReq } from "@/app/api/api";
+import { StatusSuccessCodes } from "@/utils/successStatus";
+import { getFreelancerProfileData } from "@/store/reducers/freelanceProfile";
+import { useSession } from "next-auth/react";
 
 export default function AddExperienceModal({
   isOpen,
@@ -11,9 +18,29 @@ export default function AddExperienceModal({
 }) {
   const t = useTranslations();
   const [addExperienceForm] = Form.useForm();
+  const { data }: { data: any } = useSession<any>();
+  const dispatch = useAppDispatch();
 
   function addWorkExperience(values: any) {
-    console.log(values);
+    values.start_date = dayjs(values.start_date).format("YYYY-MM-DD");
+    if (values.is_still_working) {
+      values.end_date = null;
+    } else {
+      values.end_date = dayjs(values.end_date).format("YYYY-MM-DD");
+    }
+    let url = `api/v1/client/profile/${data?.user_id}/`;
+    PatchReq(url, { experiences: [values] }).then((res: any) => {
+      if (StatusSuccessCodes.includes(res?.status)) {
+        message.success(t("updatedSuccessfully"));
+        dispatch(getFreelancerProfileData([{}, data?.user_id]));
+        addExperienceForm.resetFields();
+        closeModal();
+      } else {
+        res.errors.map((err: any) => {
+          message.error(`${err.code}:${err.detail}`);
+        });
+      }
+    });
   }
 
   return (
@@ -38,14 +65,14 @@ export default function AddExperienceModal({
           onFinish={addWorkExperience}
         >
           <Form.Item
-            name="job_title"
+            name="role"
             label={t("jobTitle")}
             rules={[{ required: true, message: t("requiredMessage") }]}
           >
             <Input placeholder={t("jonTitleex")} />
           </Form.Item>
           <Form.Item
-            name="company"
+            name="company_name"
             label={t("company")}
             rules={[{ required: true, message: t("requiredMessage") }]}
           >
@@ -53,7 +80,7 @@ export default function AddExperienceModal({
           </Form.Item>
           <div className="flex flex-row w-full gap-2 ">
             <Form.Item
-              name="from"
+              name="start_date"
               label={t("from")}
               className="w-full"
               rules={[{ required: true, message: t("requiredMessage") }]}
@@ -65,14 +92,14 @@ export default function AddExperienceModal({
               />
             </Form.Item>
             <div className="w-full">
-              <Form.Item name="to" label={t("to")} className="w-full">
+              <Form.Item name="end_date" label={t("to")} className="w-full">
                 <DatePicker
                   picker="month"
                   placeholder={t("year/month")}
                   className="w-full"
                 />
               </Form.Item>
-              <Form.Item name="till_now" valuePropName="checked">
+              <Form.Item name="is_still_working" valuePropName="checked">
                 <Checkbox>{t("tillNow")}</Checkbox>
               </Form.Item>
             </div>
