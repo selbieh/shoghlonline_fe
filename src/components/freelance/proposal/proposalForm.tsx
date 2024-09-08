fd0774f0253ea31cf24b81b50d63c84f7ab0b6e7
Karam Shaarawy <karam.shaarawy@gmail.com>
2024-09-08 04:08:32 +0300

    proposal form page

--- src/components/freelance/proposal/proposalForm.tsx
+++ src/components/freelance/proposal/proposalForm.tsx
@@ -1,22 +1,53 @@
 "use client";
-import { Button, Form, Upload } from "antd";
+import {
+  Button,
+  DatePicker,
+  Form,
+  Input,
+  InputNumber,
+  Radio,
+  Select,
+  Upload,
+} from "antd";
 import TextArea from "antd/es/input/TextArea";
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
-import { CiImageOn } from "react-icons/ci";
 import { useTranslations } from "next-intl";
+import { BsCurrencyDollar } from "react-icons/bs";
+import { FiMinusCircle } from "react-icons/fi";
 const { Dragger } = Upload;
 export default function ProposalForm() {
   const t = useTranslations();
+  const [paymentMethod, setPaymentMethod] = useState<string>("totalJob");
+
+  const suffixSelector = (
+    <Form.Item name="suffix" noStyle>
+      <Select style={{ width: 70 }} className="h-[56px] min-w-fit">
+        <Select.Option value="day">{t("day")}</Select.Option>
+        <Select.Option value="week">{t("week")}</Select.Option>
+        <Select.Option value="month">{t("month")}</Select.Option>
+        <Select.Option value="year">{t("year")}</Select.Option>
+      </Select>
+    </Form.Item>
+  );
   return (
     <div className=" w-full rounded-[12px] border-[1px] py-[30px] px-[24px] my-[20px]">
-      <Form>
+      <Form layout="vertical">
         <div className=" w-full rounded-[12px] border-[1px] py-[30px] px-[24px] my-[20px]">
-          <Form.Item>
-            <TextArea />
+          <Form.Item
+            label={<label className="font-bold">{t("coverLetter")}</label>}
+            extra={t("noMore500Letter")}
+            className="w-full h-fit"
+          >
+            <TextArea
+              style={{ minHeight: 266 }}
+              className="w-full "
+              maxLength={500}
+              placeholder={t("typeCoverLetterHere")}
+            />
           </Form.Item>
-          <Form.Item className="flex flex-col justify-center bg-[#f7f9ff]">
-            <Dragger>
+          <Form.Item className="flex flex-col justify-center bg-[#f7f9ff] ">
+            <Dragger className="h-[176px]">
               <div className="w-full ">
                 <p className="ant-upload-drag-icon flex flex-col items-center">
                   <Image
@@ -44,8 +75,152 @@ export default function ProposalForm() {
             </Dragger>
           </Form.Item>
         </div>
-        <div className=" w-full rounded-[12px] border-[1px] py-[30px] px-[24px] my-[20px]"></div>
-        <div className=" w-full rounded-[12px] border-[1px] py-[30px] px-[24px] my-[20px]"></div>
+        <div className=" w-full rounded-[12px] border-[1px] py-[30px] px-[24px] my-[20px]">
+          <Form.Item
+            label={
+              <span className="font-bold">
+                {t("chooseFavoritePaymentMethod")}
+              </span>
+            }
+          >
+            <Radio.Group
+              style={{ display: "grid" }}
+              className="w-full grid grid-cols-1 sm:grid-cols-2  "
+              onChange={(e) => {
+                setPaymentMethod(e.target.value);
+              }}
+              defaultValue={"totalJob"}
+            >
+              <div
+                className={
+                  paymentMethod === "totalJob"
+                    ? "max-w-[533px] max-h-[96px] p-[12px] border-[1px] rounded-[4px] bg-[#f7f9ff]"
+                    : "max-w-[533px] max-h-[96px] p-[12px] border-[1px] rounded-[4px]"
+                }
+              >
+                <Radio key={"totalJob"} value={"totalJob"} className="w-full">
+                  <div className="px-2">
+                    <div className="pb-[10px] font-bold text-[12px]">
+                      {t("totalJob")}
+                    </div>
+                    <div className="text-[12px] text-[#665b6d]">
+                      {t("totalJobDescription")}
+                    </div>{" "}
+                  </div>
+                </Radio>
+              </div>
+              <div
+                className={
+                  paymentMethod === "steps"
+                    ? "max-w-[533px] max-h-[96px] p-[12px] border-[1px] rounded-[4px] bg-[#f7f9ff]"
+                    : "max-w-[533px] max-h-[96px] p-[12px] border-[1px] rounded-[4px]"
+                }
+              >
+                <Radio key={"steps"} value={"steps"} className="w-full">
+                  <div className="px-2">
+                    <div className="pb-[10px] font-bold text-[12px]">
+                      {t("stepsJob")}
+                    </div>
+                    <div className="text-[12px] text-[#665b6d]">
+                      {t("projectStepsDescription")}
+                    </div>{" "}
+                  </div>
+                </Radio>
+              </div>
+            </Radio.Group>
+          </Form.Item>
+          {paymentMethod === "totalJob" && (
+            <Form.Item
+              label={<span className="font-bold">{t("price")}</span>}
+              className="w-full"
+            >
+              <InputNumber
+                className="w-full h-[56px]"
+                placeholder="300"
+                suffix={<BsCurrencyDollar />}
+              />
+            </Form.Item>
+          )}
+          {paymentMethod === "steps" && (
+            <>
+              <h3 className=" font-bold mb-5">{t("jobSteps")}</h3>
+              <Form.List name="steps">
+                {(fields, { add, remove }) => (
+                  <>
+                    {fields.map((key, name, ...restField) => (
+                      <Form.Item key={Math.random()}>
+                        <div>
+                          <div className="flex flex-row justify-between">
+                            <Form.Item
+                              label={t("stepDescription")}
+                              className="max-w-[487px] w-full"
+                            >
+                              <Input
+                                placeholder={t("stepDescription")}
+                                className="w-full h-[56px]"
+                              />
+                            </Form.Item>
+                            <Form.Item
+                              label={t("stepPrice")}
+                              className="max-w-[280px] w-full"
+                            >
+                              <InputNumber className="w-full h-[56px]" />
+                            </Form.Item>
+                            <Form.Item
+                              label={t("completeOn")}
+                              className="max-w-[280px] w-full"
+                            >
+                              <DatePicker className="w-full h-[56px]" />
+                            </Form.Item>
+                            <FiMinusCircle
+                              size={15}
+                              className="dynamic-delete-button cursor-pointer"
+                              onClick={() => remove(name)}
+                            />
+                          </div>
+                        </div>
+                      </Form.Item>
+                    ))}
+                    <Form.Item>
+                      <div
+                        onClick={() => add()}
+                        className="text-[14px] font-bold text-[#7179ce] cursor-pointer"
+                      >
+                        {t("addStep")} +
+                      </div>
+                    </Form.Item>
+                  </>
+                )}
+              </Form.List>
+            </>
+          )}
+          <div className="p-2 font-bold">
+            <span className=" px-10">{t("platformPercentage")}</span>
+            <span>10$</span>
+          </div>
+          <div className="p-5 font-bold">
+            <span className=" px-10">{t("yourProfit")}</span>
+            <span>290$</span>
+          </div>
+        </div>
+        <div className=" w-full rounded-[12px] border-[1px] py-[30px] px-[24px] my-[20px]">
+          <Form.Item
+            label={<span className="font-bold">{t("projectDuration")}</span>}
+            className="w-full"
+          >
+            <InputNumber
+              placeholder="0"
+              className="h-[56px] min-w-[300px]"
+              addonAfter={suffixSelector}
+            />
+          </Form.Item>
+        </div>
+        <div className="flex flex-row justify-end gap-5 px-2">
+          <Button type="primary" className="h-[34px] w-[84px]">
+            {t("apply")}
+          </Button>
+          <Button className="h-[34px] w-[84px]">{t("cancel")}</Button>
+        </div>
       </Form>
     </div>
   );
