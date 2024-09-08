545c5b312d6da9b32498f49314189f983905ede3
Karam Shaarawy <karam.shaarawy@gmail.com>
2024-09-08 11:08:38 +0300

    redirect to freelance or client page if user is logged in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,15 @@
+"use client";
 import Hero from "@/components/langingPage/hero";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
+  const { status }: { status: any } = useSession<any>();
+  const router = useRouter();
+  useEffect(() => {
+    status === "authenticated" ? router.push("/clientOrFreelance") : "";
+  }, [status]);
   return (
     <main className="flex min-h-screen flex-col justify-between">
       <Hero></Hero>
