48fa938eff28478ae1697c4aea7d127613df6a36
Karam Shaarawy <karam.shaarawy@gmail.com>
2024-09-21 16:25:56 +0300

    vacancy response type

--- src/utils/types/sliceInitialStates/IFreelanceInitialState.ts
+++ src/utils/types/sliceInitialStates/IFreelanceInitialState.ts
@@ -1,7 +1,32 @@
+import { AvailableSkill } from "./IFreelancerProfile";
+
 export interface freelanceInitialState {
   vacancies: vacancy[];
   getVacanciesError: any;
   getVacanciesLoading: boolean;
 }
 
-interface vacancy {}
+interface vacancy {
+  id: number;
+  title: string;
+  description: string;
+  location: string;
+  job_type: number;
+  skills: AvailableSkill[];
+  salary: string;
+  posted_by: PostedBy;
+  applied_count: string;
+  created_at: string;
+  modified_at: string;
+}
+
+interface PostedBy {
+  id: number;
+  mobile: string;
+  email: string;
+  first_name: string;
+  last_name: string;
+  is_superuser: boolean;
+  is_active: boolean;
+  groups: number[];
+}
