8eae47c928f270e1ef07cab804521df5b85f9785
Karam Shaarawy <karam.shaarawy@gmail.com>
2024-09-24 11:22:59 +0300

    integrating bookmarked vacancies end point

--- src/components/freelance/homePage/bookmarkedGigs.tsx
+++ src/components/freelance/homePage/bookmarkedGigs.tsx
@@ -1,60 +1,84 @@
-import { Button, Dropdown, Input, MenuProps, Space } from "antd";
+import { Button, Divider, Select, Skeleton } from "antd";
 import { useTranslations } from "next-intl";
-import React, { Fragment } from "react";
-import { CiSearch } from "react-icons/ci";
-import { IoIosArrowDown } from "react-icons/io";
+import React, { Fragment, useEffect } from "react";
 import GigOffer from "./gigOffer";
+import { RootState, useAppDispatch } from "@/store/rootReducer";
+import { useSelector } from "react-redux";
+import { getBookmarkedVacancies } from "@/store/reducers/freelanceSlice";
 
 export default function BookmarkedGigs() {
-  const t = useTranslations();
+  const {
+    bookmarkedVacancies,
+    getBookmarkedVacanciesError,
+    getBookmarkedVacanciesLoading,
+  } = useSelector((state: RootState) => state.freelance);
 
-  const items: MenuProps["items"] = [
+  const t = useTranslations();
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(getBookmarkedVacancies({}));
+  }, []);
+
+  const items = [
     {
       label: t("newest"),
-      key: "1",
+      value: "newest",
+    },
+    {
+      label: t("oldest"),
+      value: "oldest",
+    },
+    {
+      label: t("priceUp"),
+      value: "price up",
+    },
+    {
+      label: t("priceDown"),
+      value: "price down",
     },
   ];
 
   function sortBy(e: any) {
-    console.log("key", e);
+    dispatch(getBookmarkedVacancies({ params: { ordering: e } }));
   }
   return (
     <Fragment>
-      <div>{t("gigsSuitsYouTab")}</div>
-      <div className="w-full m-5 ">
-        <Input
-          className="h-[56px]"
-          placeholder={t("search")}
-          prefix={<CiSearch size={20} />}
-        />
-      </div>
+      {/* <div>{t("gigsSuitsYouTab")}</div> */}
 
       <div className="w-full rounded-[12px] py-[26px] border-[1px] border-[#e0e1e6] m-5 p-5">
         <div className="px-10 border-b-[1px] border-b-[#e0e1e6] pb-5 flex gap-2 items-center">
           <span>{t("sortBy")}</span>
-          <Dropdown menu={{ items, onClick: (e) => sortBy(e) }}>
-            <Space className=" w-[86px] h-[40px] border-[1px] border-[#e0e1e6] rounded-[12px] p-[10px]">
-              الاحدث
-              <IoIosArrowDown size={18} />
-            </Space>
-          </Dropdown>
+          <Select
+            options={items}
+            defaultValue={"newest"}
+            onSelect={sortBy}
+            className="min-w-[120px]"
+          />
         </div>
-        {/* <GigOffer data={null} />
-        <GigOffer data={null} />
-        <GigOffer data={null} />
-        <GigOffer data={null} />
-        <GigOffer data={null} />
-        <GigOffer data={null} />
-        <GigOffer data={null} />
-        <GigOffer data={null} /> */}
+        {getBookmarkedVacanciesLoading ? (
+          <>
+            <Skeleton active className="w-full h-[339px] my-5" />
+            <Divider className="bg-[#e7e8ec]" />
+          </>
+        ) : (
+          <>
+            {bookmarkedVacancies?.results?.map((vacancy: any) => {
+              return <GigOffer key={vacancy.id} data={vacancy} />;
+            })}
+          </>
+        )}
+
         <div className="w-full flex items-center justify-center">
-          <Button
-            style={{ height: "34px" }}
-            className="w-[95px] h-[34px] rounded-[6px] py-[8px] px-[12]"
-            type="primary"
-          >
-            {t("more")}
-          </Button>
+          {bookmarkedVacancies?.next && (
+            <Button
+              style={{ height: "34px" }}
+              className="w-[95px] h-[34px] rounded-[6px] py-[8px] px-[12]"
+              type="primary"
+            >
+              {t("more")}
+            </Button>
+          )}
         </div>
       </div>
     </Fragment>
