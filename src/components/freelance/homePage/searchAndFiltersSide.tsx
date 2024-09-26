ee23d47f1a2bfd13de50a1c40f143117fe3c6770
Karam Shaarawy <karam.shaarawy@gmail.com>
2024-09-26 13:51:36 +0300

    fix: styling

--- src/components/freelance/homePage/searchAndFiltersSide.tsx
+++ src/components/freelance/homePage/searchAndFiltersSide.tsx
@@ -3,17 +3,13 @@ import {
   getAvailableServices,
   getAvailableSkills,
 } from "@/store/reducers/freelanceProfile";
-import {
-  freelanceActions,
-  getVacancies,
-} from "@/store/reducers/freelanceSlice";
+import { freelanceActions } from "@/store/reducers/freelanceSlice";
 import { RootState, useAppDispatch } from "@/store/rootReducer";
 import {
   Cascader,
   Checkbox,
   ConfigProvider,
   Form,
-  Input,
   Select,
   Slider,
   Spin,
@@ -123,242 +119,278 @@ export default function SearchAndFiltersSide() {
           {t("clear")}
         </span>
       </div>
-      <Form layout="vertical" form={filterForm} onFinish={finishForm}>
-        <div className="flex flex-col gap-3">
-          <div className="w-[286px] h-[fit] py-[16px] px-[24px] rounded-[12px] border-[1px] bg-[#fdfdfe]">
-            <Form.Item label={t("categories")} name="services">
-              <ConfigProvider
-                theme={{
-                  token: {
-                    controlHeight: 40,
-                  },
-                }}
+      <ConfigProvider
+        theme={{
+          token: {
+            controlHeight: 40,
+          },
+        }}
+      >
+        <Form layout="vertical" form={filterForm} onFinish={finishForm}>
+          <div className="flex flex-col gap-3">
+            <div className="w-[286px] h-[fit] py-[16px] px-[24px] rounded-[12px] border-[1px] bg-[#fdfdfe]">
+              <Form.Item
+                label={
+                  <span className=" font-bold text-[12px]">
+                    {t("categories")}
+                  </span>
+                }
+                name="services"
               >
-                <Cascader
-                  placeholder={t("choseServices")}
-                  multiple
+                <ConfigProvider
+                  theme={{
+                    token: {
+                      controlHeight: 40,
+                    },
+                  }}
+                >
+                  <Cascader
+                    placeholder={t("choseServices")}
+                    multiple
+                    showSearch
+                    removeIcon={<CiCircleRemove size={25} />}
+                    className=""
+                    id="chooseService"
+                    allowClear
+                    options={availableServices}
+                    expandTrigger="hover"
+                    showCheckedStrategy={Cascader.SHOW_CHILD}
+                    onChange={() => {
+                      filterForm.submit();
+                    }}
+                  />
+                </ConfigProvider>
+              </Form.Item>
+            </div>
+            <div className="w-[286px] h-fit py-[16px] px-[24px] rounded-[12px] border-[1px] bg-[#fdfdfe]">
+              <div className="w-full flex flex-row gap-[170px]  my-3">
+                <span className=" font-bold text-[12px]">
+                  {t("experienceLabel")}
+                </span>
+                <span
+                  className="text-[#1b3dbc] text-[14px] cursor-pointer"
+                  onClick={() => {
+                    filterForm.setFieldValue("experience", undefined);
+                    filterForm.submit();
+                  }}
+                >
+                  {t("clear")}
+                </span>
+              </div>
+              <Form.Item name="experience">
+                <Checkbox.Group
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    gap: "10px",
+                  }}
+                  options={options}
+                  onChange={(e) => {
+                    filterForm.submit();
+                  }}
+                />
+              </Form.Item>
+            </div>
+            <div className="w-[286px] h-fit py-[16px] px-[24px] rounded-[12px] border-[1px] bg-[#fdfdfe]">
+              <Form.Item
+                label={
+                  <span className=" font-bold text-[12px]">{t("skills")}</span>
+                }
+                name="skills"
+              >
+                <Select
+                  placeholder={t("search")}
                   removeIcon={<CiCircleRemove size={25} />}
-                  className=""
-                  id="chooseService"
+                  className="min-h-[56px]"
+                  maxCount={15}
+                  id="chooseSkill"
+                  mode="multiple"
                   allowClear
-                  options={availableServices}
-                  expandTrigger="hover"
-                  showCheckedStrategy={Cascader.SHOW_CHILD}
+                  filterOption={false}
+                  style={{ width: "100%" }}
+                  onSearch={searchForSkill}
+                  loading={loadingGetAvailableSkills}
+                  options={skillsList}
+                  onBlur={() => {
+                    dispatch(getAvailableSkills({}));
+                  }}
+                  defaultValue={[]}
+                  onChange={() => {
+                    filterForm.submit();
+                  }}
+                  notFoundContent={
+                    loadingGetAvailableSkills ? (
+                      <Spin size="small" />
+                    ) : (
+                      <div>{t("noMatchedSkills")}</div>
+                    )
+                  }
+                />
+              </Form.Item>
+            </div>
+            <div className="w-[286px] h-fit py-[16px] px-[24px] rounded-[12px] border-[1px] bg-[#fdfdfe]">
+              <Form.Item
+                label={
+                  <span className=" font-bold text-[12px]">
+                    {t("location")}
+                  </span>
+                }
+                name="location"
+              >
+                <Select
+                  onSelect={() => {
+                    filterForm.submit();
+                  }}
+                />
+              </Form.Item>
+            </div>
+            <div className="max-w-[286px] min-h-[144px] py-[16px] px-[24px] rounded-[12px] border-[1px] bg-[#fdfdfe]">
+              <div className="w-full flex flex-row gap-[130px]  my-3">
+                <span className=" font-bold text-[12px]">
+                  {t("projectType")}
+                </span>
+                <span
+                  className="text-[#1b3dbc] text-[14px] cursor-pointer"
+                  onClick={() => {
+                    filterForm.setFieldValue("jop_type", undefined);
+                    filterForm.submit();
+                  }}
+                >
+                  {t("clear")}
+                </span>
+              </div>
+              <Form.Item name="jop_type">
+                <Checkbox.Group
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    gap: "10px",
+                  }}
+                  options={projectTypeOptions}
+                  onChange={(e) => {
+                    filterForm.submit();
+                  }}
+                />
+              </Form.Item>
+            </div>
+            <div className="max-w-[286px] h-fit py-[16px] px-[24px] rounded-[12px] border-[1px] bg-[#fdfdfe] relative">
+              <div className="w-full flex flex-row gap-[106px]  my-3">
+                <span className=" font-bold text-[12px]">
+                  {t("fixedPrice")}
+                </span>
+                <div className="flex flex-row gap-1">
+                  <span
+                    className="text-[#1b3dbc] text-[14px] cursor-pointer "
+                    onClick={() => {
+                      filterForm.submit();
+                    }}
+                  >
+                    {t("apply1")}
+                  </span>
+
+                  <span
+                    className="text-[#1b3dbc] text-[14px] cursor-pointer "
+                    onClick={() => {
+                      filterForm.setFieldValue("fixed_price", undefined);
+                      filterForm.submit();
+                    }}
+                  >
+                    {t("clear")}
+                  </span>
+                </div>
+              </div>
+              <Form.Item name="fixed_price">
+                <Slider
+                  reverse
+                  max={1000}
+                  min={0}
+                  range
+                  step={1}
+                  defaultValue={[0, 0]}
                   onChange={() => {
                     filterForm.submit();
                   }}
                 />
-              </ConfigProvider>
-            </Form.Item>
-          </div>
-          <div className="w-[286px] h-fit py-[16px] px-[24px] rounded-[12px] border-[1px] bg-[#fdfdfe]">
-            <div className="w-full flex flex-row gap-[170px]  my-3">
-              <span>{t("experienceLabel")}</span>
-              <span
-                className="text-[#1b3dbc] text-[14px] cursor-pointer"
-                onClick={() => {
-                  filterForm.setFieldValue("experience", undefined);
-                  filterForm.submit();
-                }}
-              >
-                {t("clear")}
-              </span>
+              </Form.Item>
+              <div className="flex flex-row justify-between absolute right-[35px] top-[60px] w-[220px]">
+                <span className="text-[9px]">{t("least")}</span>
+                <span className="text-[9px]">{t("highest")}</span>
+              </div>
             </div>
-            <Form.Item name="experience">
-              <Checkbox.Group
-                style={{
-                  display: "flex",
-                  flexDirection: "column",
-                  gap: "10px",
-                }}
-                options={options}
-                onChange={(e) => {
-                  filterForm.submit();
-                }}
-              />
-            </Form.Item>
-          </div>
-          <div className="w-[286px] h-fit py-[16px] px-[24px] rounded-[12px] border-[1px] bg-[#fdfdfe]">
-            <Form.Item label={t("skills")} name="skills">
-              <Select
-                placeholder={t("search")}
-                removeIcon={<CiCircleRemove size={25} />}
-                className="min-h-[56px]"
-                maxCount={15}
-                id="chooseSkill"
-                mode="multiple"
-                allowClear
-                filterOption={false}
-                style={{ width: "100%" }}
-                onSearch={searchForSkill}
-                loading={loadingGetAvailableSkills}
-                options={skillsList}
-                onBlur={() => {
-                  dispatch(getAvailableSkills({}));
-                }}
-                defaultValue={[]}
-                onChange={() => {
-                  filterForm.submit();
-                }}
-                notFoundContent={
-                  loadingGetAvailableSkills ? (
-                    <Spin size="small" />
-                  ) : (
-                    <div>{t("noMatchedSkills")}</div>
-                  )
-                }
-              />
-            </Form.Item>
-          </div>
-          <div className="w-[286px] h-fit py-[16px] px-[24px] rounded-[12px] border-[1px] bg-[#fdfdfe]">
-            <Form.Item label={t("location")} name="location">
-              <Select
-                onSelect={() => {
-                  filterForm.submit();
-                }}
-              />
-            </Form.Item>
-          </div>
-          <div className="max-w-[286px] min-h-[144px] py-[16px] px-[24px] rounded-[12px] border-[1px] bg-[#fdfdfe]">
-            <div className="w-full flex flex-row gap-[130px]  my-3">
-              <span>{t("projectType")}</span>
-              <span
-                className="text-[#1b3dbc] text-[14px] cursor-pointer"
-                onClick={() => {
-                  filterForm.setFieldValue("jop_type", undefined);
-                  filterForm.submit();
-                }}
-              >
-                {t("clear")}
-              </span>
-            </div>
-            <Form.Item name="jop_type">
-              <Checkbox.Group
-                style={{
-                  display: "flex",
-                  flexDirection: "column",
-                  gap: "10px",
-                }}
-                options={projectTypeOptions}
-                onChange={(e) => {
-                  filterForm.submit();
-                }}
-              />
-            </Form.Item>
-          </div>
-          <div className="max-w-[286px] h-fit py-[16px] px-[24px] rounded-[12px] border-[1px] bg-[#fdfdfe]">
-            <div className="w-full flex flex-row gap-[106px]  my-3">
-              <span>{t("fixedPrice")}</span>
-              <div className="flex flex-row gap-1">
-                <span
-                  className="text-[#1b3dbc] text-[14px] cursor-pointer "
-                  onClick={() => {
+            <div className="max-w-[286px] h-fit py-[16px] px-[24px] rounded-[12px] border-[1px] bg-[#fdfdfe] relative">
+              <div className="w-full flex flex-row gap-[106px]  my-3">
+                <span className=" font-bold text-[12px]">{t("hourPrice")}</span>
+                <div className="flex flex-row gap-1">
+                  <span
+                    className="text-[#1b3dbc] text-[14px] cursor-pointer "
+                    onClick={() => {
+                      filterForm.submit();
+                    }}
+                  >
+                    {t("apply1")}
+                  </span>
+
+                  <span
+                    className="text-[#1b3dbc] text-[14px] cursor-pointer "
+                    onClick={() => {
+                      filterForm.setFieldValue("hour_price", undefined);
+                      filterForm.submit();
+                    }}
+                  >
+                    {t("clear")}
+                  </span>
+                </div>
+              </div>
+              <Form.Item name="hour_price">
+                <Slider
+                  reverse
+                  max={1000}
+                  min={0}
+                  range
+                  step={1}
+                  defaultValue={[0, 0]}
+                  onChange={(e) => {
                     filterForm.submit();
                   }}
-                >
-                  {t("apply1")}
+                />
+              </Form.Item>
+              <div className="flex flex-row justify-between absolute w-[220px] right-[35px] top-[75px]">
+                <span className="text-[9px]">{t("least")}</span>
+                <span className="text-[9px]">{t("highest")}</span>
+              </div>
+            </div>
+            <div className="max-w-[286px] h-fit py-[16px] px-[24px] rounded-[12px] border-[1px] bg-[#fdfdfe]">
+              <div className="w-full flex flex-row gap-[106px]  my-3">
+                <span className=" font-bold text-[12px]">
+                  {t("clientInfo")}
                 </span>
-
                 <span
-                  className="text-[#1b3dbc] text-[14px] cursor-pointer "
+                  className="text-[#1b3dbc] text-[14px] cursor-pointer"
                   onClick={() => {
-                    filterForm.setFieldValue("fixed_price", undefined);
+                    filterForm.setFieldValue("payment_verified", undefined);
                     filterForm.submit();
                   }}
                 >
                   {t("clear")}
                 </span>
               </div>
-            </div>
-            <Form.Item name="fixed_price">
-              <Slider
-                reverse
-                max={1000}
-                min={0}
-                range
-                step={1}
-                defaultValue={[0, 0]}
-                // onChange={(e) => {
-                //   filterForm.submit();
-                // }}
-              />
-            </Form.Item>
-            <div className="flex flex-row justify-between">
-              <span className="text-[9px]">{t("least")}</span>
-              <span className="text-[9px]">{t("highest")}</span>
-            </div>
-          </div>
-          <div className="max-w-[286px] h-fit py-[16px] px-[24px] rounded-[12px] border-[1px] bg-[#fdfdfe]">
-            <div className="w-full flex flex-row gap-[106px]  my-3">
-              <span>{t("hourPrice")}</span>
-              <div className="flex flex-row gap-1">
-                <span
-                  className="text-[#1b3dbc] text-[14px] cursor-pointer "
-                  onClick={() => {
+              <Form.Item name="payment_verified">
+                <Checkbox.Group
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    gap: "10px",
+                  }}
+                  options={clientInfoOptions}
+                  onChange={(e) => {
                     filterForm.submit();
                   }}
-                >
-                  {t("apply1")}
-                </span>
-
-                <span
-                  className="text-[#1b3dbc] text-[14px] cursor-pointer "
-                  onClick={() => {
-                    filterForm.setFieldValue("hour_price", undefined);
-                    filterForm.submit();
-                  }}
-                >
-                  {t("clear")}
-                </span>
-              </div>
-            </div>
-            <Form.Item name="hour_price">
-              <Slider
-                reverse
-                max={1000}
-                min={0}
-                range
-                step={1}
-                defaultValue={[0, 0]}
-                // onChange={(e) => {
-                //   filterForm.submit();
-                // }}
-              />
-            </Form.Item>
-            <div className="flex flex-row justify-between">
-              <span className="text-[9px]">{t("least")}</span>
-              <span className="text-[9px]">{t("highest")}</span>
+                />
+              </Form.Item>
             </div>
           </div>
-          <div className="max-w-[286px] h-fit py-[16px] px-[24px] rounded-[12px] border-[1px] bg-[#fdfdfe]">
-            <div className="w-full flex flex-row gap-[106px]  my-3">
-              <span>{t("clientInfo")}</span>
-              <span
-                className="text-[#1b3dbc] text-[14px] cursor-pointer"
-                onClick={() => {
-                  filterForm.setFieldValue("payment_verified", undefined);
-                  filterForm.submit();
-                }}
-              >
-                {t("clear")}
-              </span>
-            </div>
-            <Form.Item name="payment_verified">
-              <Checkbox.Group
-                style={{
-                  display: "flex",
-                  flexDirection: "column",
-                  gap: "10px",
-                }}
-                options={clientInfoOptions}
-                onChange={(e) => {
-                  filterForm.submit();
-                }}
-              />
-            </Form.Item>
-          </div>
-        </div>
-      </Form>
+        </Form>
+      </ConfigProvider>
     </div>
   );
 }
